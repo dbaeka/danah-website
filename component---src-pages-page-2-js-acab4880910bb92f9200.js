a347db97fc84bdd4b47daf07e620bdf85057f144
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-05-06 00:51:05 -0400

    Updates

---
+++ component---src-pages-page-2-js-acab4880910bb92f9200.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{p5nM:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),o=a("Wbzz"),c=a("vrFN"),r=a("llw9");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"Page two"}),l.a.createElement("h1",null,"Hi from the second page"),l.a.createElement("p",null,"Welcome to page 2"),l.a.createElement(o.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-acab4880910bb92f9200.js.map
