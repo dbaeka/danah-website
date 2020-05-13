22471ad3c1500ad35eabcd753e8cbeb4fdea44e7
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-05-13 18:28:57 -0400

    Updates

---
+++ js/popup.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
+
+$(document).ready(function() {
+    $('#gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        closeOnContentClick: false,
+        closeBtnInside: false,
+        mainClass: 'mfp-with-zoom mfp-img-mobile',
+        image: {
+            verticalFit: true,
+        },
+        gallery: {
+            enabled: true
+        },
+        zoom: {
+            enabled: true,
+            duration: 300, // don't foget to change the duration also in CSS
+            opener: function(element) {
+                return element.find('img');
+            }
+        }
+
+    });
+});
