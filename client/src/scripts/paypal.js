fefd79f25a12898f01310c7419f7547c8e2cabc0
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2020-11-25 08:29:20 +0000

    update

--- client/src/scripts/paypal.js
+++ client/src/scripts/paypal.js
@@ -20,4 +20,28 @@ paypal.Buttons({
             alert('Transaction completed by ' + details.payer.name.given_name + '!');
         });
     }
-}).render('.paypal-button-container');
+}).render('#paypal-button-container1');
+
+paypal.Buttons({
+    style: {
+        shape: 'rect',
+        color: 'gold',
+        layout: 'vertical',
+        label: 'paypal',
+
+    },
+    createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: '100'
+                }
+            }]
+        });
+    },
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+        });
+    }
+}).render('#paypal-button-container2');
---
+++ client/src/scripts/weglot.js
@@ -0,0 +1,3 @@
+Weglot.initialize({
+    api_key: 'wg_9bb806d7c8e03c59d3fd2111d4dd847f4'
+});
