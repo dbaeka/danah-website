25d92607931b43644d2a1b66efe2d29358395c42
Delmwin Baeka <dbaekajnr@sjtu.edu.cn>
2021-02-14 22:24:52 +0000

    updated paypal script

--- client/src/scripts/paypal.js
+++ client/src/scripts/paypal.js
@@ -6,7 +6,7 @@ paypal.Buttons({
         label: 'paypal',
 
     },
-    createOrder: function(data, actions) {
+    createOrder: function (data, actions) {
         return actions.order.create({
             purchase_units: [{
                 amount: {
@@ -15,9 +15,27 @@ paypal.Buttons({
             }]
         });
     },
-    onApprove: function(data, actions) {
-        return actions.order.capture().then(function(details) {
-            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            const data = {
+                ...details,
+            };
+
+            fetch('https://thesqtest.com/api/v1/token.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            alert('Transaction completed by ' + details.payer.name.given_name + '! Check your email');
         });
     }
 }).render('#paypal-button-container1');
@@ -30,7 +48,7 @@ paypal.Buttons({
         label: 'paypal',
 
     },
-    createOrder: function(data, actions) {
+    createOrder: function (data, actions) {
         return actions.order.create({
             purchase_units: [{
                 amount: {
@@ -39,9 +57,27 @@ paypal.Buttons({
             }]
         });
     },
-    onApprove: function(data, actions) {
-        return actions.order.capture().then(function(details) {
-            alert('Transaction completed by ' + details.payer.name.given_name + '!');
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            const data = {
+                ...details,
+            };
+
+            fetch('https://thesqtest.com/api/v1/token.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+            alert('Transaction completed by ' + details.payer.name.given_name + '! Check your email');
         });
     }
 }).render('#paypal-button-container2');
