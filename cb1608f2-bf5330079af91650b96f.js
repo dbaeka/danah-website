ba0af610be4b96eb2bb83bc24612f0effbde803f
Delmwin Baeka <delmwin@buckhill.co.uk>
2024-05-16 09:34:53 +0000

    Updates

--- cb1608f2-bf5330079af91650b96f.js
+++
@@ -1,3 +0,0 @@
-/*! For license information please see cb1608f2-bf5330079af91650b96f.js.LICENSE.txt */
-(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{wHSu:function(c,n,o){"use strict";o.d(n,"a",(function(){return i}));var i={prefix:"fas",iconName:"long-arrow-alt-right",icon:[448,512,[],"f30b","M313.941 216H12c-6.627 0-12 5.373-12 12v56c0 6.627 5.373 12 12 12h301.941v46.059c0 21.382 25.851 32.09 40.971 16.971l86.059-86.059c9.373-9.373 9.373-24.569 0-33.941l-86.059-86.059c-15.119-15.119-40.971-4.411-40.971 16.971V216z"]}}}]);
-//# sourceMappingURL=cb1608f2-bf5330079af91650b96f.js.map
---
+++ component---src-pages-404-js-a86bfbd7b6025ec6407c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("vrFN"),o=n("llw9");t.default=function(){return l.a.createElement(o.a,null,l.a.createElement(u.a,{title:"404: Not found"}),l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a86bfbd7b6025ec6407c.js.map
